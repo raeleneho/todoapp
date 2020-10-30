90d6540e5f0ac9057230ea03ae5fdfb211d058ff
Raelene Ho <raeyean@gmail.com>
2020-10-30 15:42:07 +1100

    refactored forloops to forEach

--- javascript/script.js
+++ javascript/script.js
@@ -20,23 +20,31 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     // get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
+
+    this.todos.forEach(function(todo) {
       if (this.todos[i].completed === true) {
         completedTodos++;
       }
-    }
+    });
+    // if (completedTodos === totalTodos) {
+    //   // make everything false.
+    //   this.todos.forEach(function(todo) {
+    //     todo.completed = false;
+    //   });
+    //     // Case 2: Otherwise, make everything true.
+    // } else {
+    //   this.todos.forEach(function(todo) {
+    //     todo.completed = true;
+    //   });
+    // }
+    this.todos.forEach(function(todo) {
     // Case 1: if everything's true, make everything false.
-    if (completedTodos === totalTodos) {
-      // make everything false.
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-        // Case 2: Otherwise, make everything true.
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      } else {
+        todo.completed - true;
       }
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
-    }
+    });
   }
 };
 
@@ -75,9 +83,24 @@ var view = {
   displayTodos: function() { 
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+    // for (var i = 0; i < todoList.todos.length; i++) {
+    //   var todoLi = document.createElement('li');
+    //   var todo = todoList.todos[i];
+    //   var todoTextWithCompletion = '';
+
+    //   if (todo.completed === true) {
+    //     todoTextWithCompletion = '(x) ' + todo.todoText;
+    //   } else {
+    //     todoTextWithCompletion = '( ) ' + todo.todoText;
+    //   }
+    //   todoLi.id = i;
+    //   todoLi.textContent = todoTextWithCompletion;
+    //   todoLi.appendChild(this.createDeleteButton());
+    //   todosUl.appendChild(todoLi);
+    // }
+
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -85,18 +108,19 @@ var view = {
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
-      todoLi.id = i;
+
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this);
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
     return deleteButton;
-  }
+  },
   setUpEventListeners: function() {
     var todosUl = document.querySelector('ul');
     todosUl.addEventListener('click', function(event) {
